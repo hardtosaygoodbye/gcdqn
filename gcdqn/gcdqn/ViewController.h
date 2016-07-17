26a179e0aeda26491f352c8116f6a707cc6df582
admin <admin@admindemacbook-pro.local>
2016-07-17 08:07:36 +0800

    初始化项目

---
+++ gcdqn/gcdqn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gcdqn
+//
+//  Created by admin on 16/7/17.
+//  Copyright © 2016年 hardtosaygoodbye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gcdqn/gcdqn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gcdqn
+//
+//  Created by admin on 16/7/17.
+//  Copyright © 2016年 hardtosaygoodbye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
